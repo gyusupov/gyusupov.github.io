0f9257a022e5412125c65474c28504ce07f73429
ashleyparabdin <aparabdin@yahoo.com>
2015-08-11 15:40:54 -0400

    rand

---
+++ finalproj.js
@@ -0,0 +1,12 @@
+POST /oauth/request_token HTTP/1.1
+User-Agent: themattharris' HTTP Client
+Host: api.twitter.com
+Accept: */*
+Authorization: 
+        OAuth oauth_callback="http%3A%2F%2Flocalhost%2Fsign-in-with-twitter%2F",
+              oauth_consumer_key="cChZNFj6T5R0TigYB9yd1w",
+              oauth_nonce="ea9ec8429b68d6b77cd5600adbbb0456",
+              oauth_signature="F1Li3tvehgcraF8DMJ7OyxO4w9Y%3D",
+              oauth_signature_method="HMAC-SHA1",
+              oauth_timestamp="1318467427",
+              oauth_version="1.0"
